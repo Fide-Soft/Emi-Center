167fd527bb7c8e12b45e92d7b5c10570769fd8fc
pisarews <pisaqtom@gmail.com>
2020-07-02 11:15:43 +0100

    Cennik Usług Templating + Data Attributes, ConvertTo String, Decimal etc.
    /*
    MUST To create Routing
    */

--- App_Data/Models/all.generated.cs
+++ App_Data/Models/all.generated.cs
@@ -7,7 +7,7 @@ using  Umbraco.Core.Models.PublishedContent;
 using  Umbraco.Web;
 using  Umbraco.ModelsBuilder.Embedded;
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "7a4272fddae18662")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.5")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 
 // FILE: models.generated.cs
